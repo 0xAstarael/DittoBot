97b4d964c49bddac80f3e25dac78b632ddf0c2b9
Yunke Liu <102265215+0xAstarael@users.noreply.github.com>
2022-05-13 14:04:28 -0400

    Update setup.ts

--- src/discord2telegram/setup.ts
+++ src/discord2telegram/setup.ts
@@ -284,6 +284,8 @@ export function setup(
 				);
 				const tgMessageId = dittoMessage.telegramMessageId;
 
+				console.log(dittoMessage);
+				console.log(newMessage);
 				// Check if this is a pin update
 				if (!dittoMessage.pinned && newMessage.pinned) {
 					tgBot.telegram.pinChatMessage(bridge.telegram.chatId, tgMessageId);
